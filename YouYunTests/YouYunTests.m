887abbecfe771905d73183dd6c6d1b23584a468f
Ranchao Zhang <r.c.cham@gmail.com>
2014-02-28 23:22:15 -0800

    Modified gitignore

---
+++ YouYunTests/YouYunTests.m
@@ -0,0 +1,34 @@
+//
+//  YouYunTests.m
+//  YouYunTests
+//
+//  Created by Ranchao Zhang on 2/28/14.
+//  Copyright (c) 2014 Ranchao Zhang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface YouYunTests : XCTestCase
+
+@end
+
+@implementation YouYunTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
