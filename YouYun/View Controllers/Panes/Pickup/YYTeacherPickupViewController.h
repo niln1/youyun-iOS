39e514e19a57efc959d1bad138f32a37d368efba
Ni Zhihao <niz@rose-hulman.edu>
2014-08-12 21:42:18 -0700

    add teacher view

---
+++ YouYun/View Controllers/Panes/Pickup/YYTeacherPickupViewController.h
@@ -0,0 +1,13 @@
+//
+//  YYTeacherPickupViewController.h
+//  YouYun
+//
+//  Created by Zhihao Ni on 7/31/14.
+//  Copyright (c) 2014 Ranchao Zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YYTeacherPickupViewController : UIViewController
+
+@end
